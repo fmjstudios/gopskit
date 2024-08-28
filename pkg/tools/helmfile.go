aaf228a46669f8501478997c9c51f7100b91abd5
Maximilian Gindorfer <maximilian.gindorfer@delta4x4.com>
2024-08-28 14:10:27 +0200

    chore(pkg/tools): remove `helmfile` and `certificates`
    Remove both Go modules, because they prevented Bazel from building. Will be looking for alternatives.

--- pkg/tools/helmfile.go
+++ pkg/tools/helmfile.go
@@ -1,16 +1,11 @@
 package tools
 
-import (
-	"github.com/helmfile/helmfile/pkg/app"
-	"github.com/helmfile/helmfile/pkg/config"
-)
-
 // WriteValues implements Helmfile's 'write-values' command
-func WriteValues(global *config.GlobalImpl) error {
-	writeValuesOptions := config.NewWriteValuesOptions()
-	impl := config.NewWriteValuesImpl(global, writeValuesOptions)
+// func WriteValues(global *config.GlobalImpl) error {
+// 	writeValuesOptions := config.NewWriteValuesOptions()
+// 	impl := config.NewWriteValuesImpl(global, writeValuesOptions)
 
-	h := app.New(impl)
-	h.WriteValues(impl)
-	return nil
-}
+// 	h := app.New(impl)
+// 	h.WriteValues(impl)
+// 	return nil
+// }
--- pkg/tools/smallstep.go
+++ pkg/tools/smallstep.go
@@ -1,18 +1,13 @@
 package tools
 
-import (
-	"github.com/smallstep/certificates/cas/apiv1"
-	"github.com/smallstep/certificates/pki"
-)
-
 // ref: https://github.com/smallstep/cli/blob/master/command/ca/init.go
 
 // NewValues returns a new PKI configuration for the Smallstep Certificate Authority
-func NewValues() (*pki.PKI, error) {
-	p, err := pki.New(apiv1.Options{})
-	if err != nil {
-		return nil, err
-	}
+// func NewValues() (*pki.PKI, error) {
+// 	p, err := pki.New(apiv1.Options{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return p, nil
-}
+// 	return p, nil
+// }
