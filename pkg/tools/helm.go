17e00e526227ed0f79ee80672685bc0205dc5961
FMJdev <info@fmj.dev>
2024-08-30 07:31:34 +0200

    feat(pkg/tools): add helm functions

--- pkg/tools/helm.go
+++ pkg/tools/helm.go
@@ -1,16 +1,274 @@
 package tools
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/fmjstudios/gopskit/pkg/filesystem"
+	"github.com/go-resty/resty/v2"
+	"github.com/vmware-labs/yaml-jsonpath/pkg/yamlpath"
+	"golang.org/x/mod/semver"
+	"gopkg.in/yaml.v3"
+)
+
 // HelmPlugin represents a Helm plugin required for gopskit to work
-type HelmPlugin string
+type HelmPlugin int
+
+const (
+	diff HelmPlugin = iota
+	secrets
+)
+
+// String implements the fmt.Stringer interface for the new HelmPlugin type
+func (p HelmPlugin) String() string {
+	return [...]string{"diff", "secrets"}[p]
+}
+
+// Index makes the current HelmPlugin index
+func (p HelmPlugin) Index() int {
+	return int(p)
+}
 
 var (
-	pluginDiff    HelmPlugin = "diff"
-	pluginSecrets HelmPlugin = "secrets"
-
-	// HelmPlugins is a list of Helm Plugins
-	plugins    = []HelmPlugin{pluginDiff, pluginSecrets}
-	pluginURLs = map[HelmPlugin]string{
-		pluginDiff:    "https://github.com/databus23/helm-diff",
-		pluginSecrets: "https://github.com/jkroepke/helm-secrets",
+	helmPlugins        = []HelmPlugin{diff, secrets}
+	helmPluginsRepoMap = map[HelmPlugin]string{
+		diff:    "https://github.com/databus23/helm-diff",
+		secrets: "https://github.com/jkroepke/helm-secrets",
 	}
 )
+
+// ValidateHelmPlugins checks if the required Helm Plugins "diff" and "secrets" are currently installed
+func ValidateHelmPlugins(plugins ...HelmPlugin) error {
+	// use (built-in) const if no args are passed
+	if len(plugins) == 0 {
+		plugins = helmPlugins
+	}
+
+	for _, v := range plugins {
+		switch v {
+		case diff:
+			_, err := Exec("helm", diff.String(), "version")
+			if err != nil {
+				return err
+			}
+		case secrets:
+			_, err := Exec("helm", secrets.String(), "--version")
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// HelmPluginVersion retrieves the versions for all or some of the required Helm Plugins
+func HelmPluginVersion(plugins ...HelmPlugin) (map[HelmPlugin]string, error) {
+	var diffVer, secretsVer string
+
+	// use (built-in) const if no args are passed
+	if len(plugins) == 0 {
+		plugins = helmPlugins
+	}
+
+	// sanity
+	if err := ValidateHelmPlugins(plugins...); err != nil {
+		return nil, err
+	}
+
+	// we can safely ignore errors if we get here
+	for _, v := range plugins {
+		switch v {
+		case diff:
+			res, _ := Exec("helm", diff.String(), "version")
+			diffVer = res.StdOut
+		case secrets:
+			res, _ := Exec("helm", secrets.String(), "--version")
+			secretsVer = res.StdOut
+		}
+	}
+
+	return map[HelmPlugin]string{
+		diff:    diffVer,
+		secrets: secretsVer,
+	}, nil
+}
+
+// HelmPluginRequiresUpdate determines if an update to a plugin is required
+func HelmPluginRequiresUpdate(token string, plugins ...HelmPlugin) (map[HelmPlugin]bool, error) {
+	req := make(map[HelmPlugin]bool)
+
+	httpC := resty.New()
+	if token != "" {
+		httpC.SetAuthToken(token)
+	}
+
+	// use (built-in) const if no args are passed
+	if len(plugins) == 0 {
+		plugins = helmPlugins
+	}
+
+	// sanity
+	if err := ValidateHelmPlugins(plugins...); err != nil {
+		return nil, err
+	}
+
+	for _, v := range plugins {
+		pluginVersion, _ := HelmPluginVersion(v)
+		remoteTag, err := latestGitHubRelease(httpC, helmPluginsRepoMap[v])
+		if err != nil {
+			return nil, err
+		}
+
+		remoteVer := versionFromTag(remoteTag.TagName)
+		req[v] = semver.Compare(pluginVersion[v], remoteVer) == -1
+	}
+
+	return req, nil
+}
+
+// func HelmPluginInstall installs a Helm Plugin from it's remote source
+func HelmPluginInstall(p HelmPlugin, version string) error {
+	if !semver.IsValid(version) {
+		return fmt.Errorf("cannot install Helm Plugin %s at invalid version: %v", p.String(), version)
+	}
+
+	installArgs := []string{"helm", "plugin", "install", p.String(), helmPluginsRepoMap[p], "--version", tagFromVersion(version)}
+	_, err := Exec(installArgs...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// func HelmPluginUninstall uninstalls a Helm Plugin
+func HelmPluginUninstall(p HelmPlugin) error {
+	err := ValidateHelmPlugins(p)
+	if err != nil {
+		return err
+	}
+
+	uninstallArgs := []string{"helm", "plugin", "uninstall", p.String()}
+	_, err = Exec(uninstallArgs...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type FileState int
+
+const (
+	encrypted FileState = iota
+	decrypted
+)
+
+// // String implements the fmt.Stringer interface for the new FileState type
+func (f FileState) String() string {
+	return [...]string{"encrypted", "decrypted"}[int(f)]
+}
+
+// A SOPS-encrypted file always has a 'sops' keys if it's currently encrypted
+type SOPSContent struct {
+	SOPS yaml.Node `json:"sops" yaml:"sops"`
+}
+
+// GetFileState checks the contents of a file for existing SOPS encryption and returns the
+// current state of the file
+func GetFileState(path string) (FileState, error) {
+	var c SOPSContent
+
+	if ok := filesystem.CheckIfExists(path); !ok {
+		return -1, fmt.Errorf("cannot get state of non-existing file: %s", path)
+	}
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return -1, err
+	}
+
+	if err := yaml.Unmarshal(content, c); err != nil {
+		return decrypted, nil
+	}
+
+	return encrypted, nil
+}
+
+// EncryptFile encrypts a file using the Helm Secrets Plugin
+func EncryptFile(path string) error {
+	if ok := filesystem.CheckIfExists(path); !ok {
+		return fmt.Errorf("cannot encrypt non-existing file: %s", path)
+	}
+
+	args := []string{"helm", "secrets", "encrypt", "-i", path}
+	_, err := Exec(args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DecryptFile decrypts a file using the Helm Secrets Plugin
+func DecryptFile(path string) error {
+	if ok := filesystem.CheckIfExists(path); !ok {
+		return fmt.Errorf("cannot decrypt non-existing file: %s", path)
+	}
+
+	args := []string{"helm", "secrets", "encrypt", "-i", path}
+	_, err := Exec(args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetSecretValue
+func GetSecretValue(path, jsonPath string, unencrypted bool) (string, error) {
+	var data yaml.Node
+
+	if ok := filesystem.CheckIfExists(path); !ok {
+		return "", fmt.Errorf("cannot geg value from non-existing file: %s", path)
+	}
+
+	state, err := GetFileState(path)
+	if err != nil {
+		return "", err
+	}
+
+	if state == encrypted {
+		if err := DecryptFile(path); err != nil {
+			return "", err
+		}
+	}
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	if err := yaml.Unmarshal(content, data); err != nil {
+		return "", err
+	}
+
+	yp, err := yamlpath.NewPath(jsonPath)
+	if err != nil {
+		return "", err
+	}
+
+	nodes, err := yp.Find(&data)
+	if err != nil {
+		return "", err
+	}
+
+	if !unencrypted {
+		if err := EncryptFile(path); err != nil {
+			return "", err
+		}
+	}
+
+	return nodes[0].Value, nil
+}
--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -6,52 +6,53 @@
 package tools
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/go-resty/resty/v2"
 )
 
-// Tool represents an executable the gopskit package 'tools' depends on
-type Tool string
-
-// String implements the Stringer interface for Tool
-func (t *Tool) String() string {
-	return string(*t)
-}
+// Executables represents and executables that either a Tool or another required programs depends on
+type Executable int
 
 const (
-	gitHubURL = "https://github.com/"
+	kubectl Executable = iota
+	helm
+	helmfile
+	stepCA
+	kustomize
 )
+
+// String implements the fmt.Stringer interface for the new Executable type
+func (e Executable) String() string {
+	return [...]string{"kubectl", "helm", "helmfile", "step-ca", "kustomize"}[e]
+}
+
+// Index makes the current Executable index retrievable
+func (e Executable) Index() int {
+	return int(e)
+}
 
 var (
-	// represents the kubectl CLI
-	Kubectl Tool = "kubectl"
-
-	// the helmfile CLI
-	Helmfile Tool = "helmfile"
-
-	// the Helm CLI
-	Helm Tool = "helm"
-
-	// ref: https://github.com/smallstep/cli
-	StepCA Tool = "step"
-
-	// the kustomize CLI
-	Kustomize Tool = "kustomize"
-
-	tools = []Tool{Kubectl, Helmfile, Helm, StepCA, Kustomize}
+	githubURL   = "https://github.com/"
+	executables = []Executable{kubectl, helm, helmfile, stepCA, kustomize}
 )
 
-// Find checks the system for the required tools. It returns the first error that occurs during the search.
-// If any error is returned the map is nil.
-func Find() (map[Tool]string, error) {
-	t := make(map[Tool]string)
+// Find checks the system for the required executables. It returns the first error that occurs during
+// the search, thereby setting the map return value to nil.
+func Find() (map[Executable]string, error) {
+	t := make(map[Executable]string)
 
-	for _, v := range tools {
+	for _, v := range executables {
 		path, err := LookPath(v.String())
 		if err != nil {
 			return nil, fmt.Errorf("could not find tool: %s. error: %v", v, err)
@@ -75,6 +76,80 @@ func LookPath(path string) (string, error) {
 	return path, err
 }
 
+// execResult is the result of executing a command via Exec
+type execResult struct {
+	Command string
+	Bin     string
+	Args    []string
+	StdOut  string
+	StdErr  string
+	RC      int
+	Error   error
+}
+
+// Exec executes a command within the current environment (akin to shell) and either returns an
+// error if any occurred of an execResult object containing the exit code a copy of the error,
+// as well as the string output of the command for StdOut and StdErr
+//
+// If the program in the command isn't installed we fail fast instead of trying to execute a command
+// that does not exist on the system.
+//
+// Exec allows for the cancellation of a running command via CTRL+C (SIGINT).
+func Exec(command ...string) (*execResult, error) {
+	var args []string
+	var stdOut, stdErr bytes.Buffer
+	var cmdErr error = nil
+
+	args = command
+	if len(command) <= 1 {
+		args = strings.Fields(command[0])
+	}
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
+
+	// allow CTRL+C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGINT)
+	go func() {
+		<-c
+		cancelCtx()
+		fmt.Printf("execution of command: '%s' was cancelled via CTRL+C", shortenedCommand(args...))
+	}()
+
+	// sanity
+	bin, err := LookPath(args[0])
+	if err != nil {
+		return nil, fmt.Errorf("no such binary or executable in PATH: %s. cannot execute non-existent program", bin)
+	}
+
+	cmd := exec.CommandContext(ctx, bin, args[1:]...)
+	cmd.Stderr = io.MultiWriter(os.Stderr, &stdErr)
+	cmd.Stdout = io.MultiWriter(os.Stdout, &stdOut)
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			cmdErr = exitErr
+		} else {
+			cmdErr = err
+		}
+	}
+
+	return &execResult{
+		Command: shortenedCommand(args...),
+		Bin:     bin,
+		Args:    args[1:],
+		StdOut:  stdOut.String(),
+		StdErr:  stdErr.String(),
+		RC:      cmd.ProcessState.ExitCode(),
+		Error:   cmdErr,
+	}, nil
+}
+
 // releaseResponse represents a GitHub API response
 type releaseResponse struct {
 	Id      int    `json:"id"`
@@ -83,7 +158,9 @@ type releaseResponse struct {
 	Body    string `json:"body"`
 }
 
-// latestGitHubRelease fetches the latest release data for a given GitHub Repository
+// latestGitHubRelease fetches the latest release data for a given GitHub Repository. If an authenticated request
+// should be made, the passed in client should have a Bearer Auth Token set for every request. To do so, call the
+// client.SetAuthToken method before passing it in
 func latestGitHubRelease(client *resty.Client, url string) (*releaseResponse, error) {
 	req := client.R().
 		SetHeader("Accept", "application/vnd.github+json").
@@ -111,12 +188,42 @@ func latestGitHubRelease(client *resty.Client, url string) (*releaseResponse, er
 func parseGitHubURL(url string) (string, string, error) {
 	var s string
 
-	if strings.Contains(url, gitHubURL) {
-		s = strings.TrimPrefix(url, gitHubURL)
+	if strings.Contains(url, githubURL) {
+		s = strings.TrimPrefix(url, githubURL)
 	} else {
 		return "", "", fmt.Errorf("cannot parse GitHub URL for non-GitHub-URL: %s", url)
 	}
 
 	ss := strings.Split(s, "/")
 	return ss[0], ss[1], nil
+}
+
+// shortenedCommand creates a short representation of a command in order to produce valuable
+// debug or info messages without exposing all of the arguments passed in. The func also handles
+// cases where entire strings are passed in by splitting the argument if it's only a single one
+func shortenedCommand(s ...string) string {
+	var strs []string
+	if len(s) == 1 {
+		strs = strings.Fields(s[0])
+	}
+
+	if len(strs) <= 2 {
+		return fmt.Sprintf("%s ...", strs)
+	} else {
+		return fmt.Sprintf("%s ...", strs[:3])
+	}
+}
+
+// Trim the v from the GitHub Tag
+func versionFromTag(tag string) string {
+	return strings.TrimPrefix("v", tag)
+}
+
+// Add a 'v' to a SemVer version if needed
+func tagFromVersion(version string) string {
+	if strings.HasPrefix(version, "v") {
+		return version
+	}
+
+	return fmt.Sprintf("v%s", version)
 }
