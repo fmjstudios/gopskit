e0d6bea168663325855e240a19b8e1ec453ffa3b
FMJdev <info@fmj.dev>
2024-09-23 00:15:52 +0200

    chore(scripts/lib): add required `lib` shell scripts
    Add the required shell scripts the new `release.sh` relies on.

---
+++ scripts/lib/paths.sh
@@ -0,0 +1,37 @@
+# shellcheck shell=bash
+
+# BASH functions to obtain correct paths.
+
+#######################################
+# Determine the repository's paths.
+#
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   The given filesystem path.
+#######################################
+
+# Return the repository's root path
+lib::paths::root() {
+  path=$(git rev-parse --show-toplevel)
+
+  echo "${path%/}"
+}
+
+# Return the secrets path
+lib::paths::secrets() {
+  root_path=$(lib::paths::root)
+
+  path="${root_path}/secrets"
+  echo "${path%/}"
+}
+
+# Return the k8s path
+lib::paths::k8s() {
+  root_path=$(lib::paths::root)
+
+  path="${root_path}/k8s"
+  echo "${path%/}"
+}
