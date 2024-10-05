a59b2d52594311c0a1a0b69bee8cfd6a526629d7
FMJdev <info@fmj.dev>
2024-10-05 11:14:18 +0200

    perf(waltr): update the central `app` package
    - add support for asynchronicity
    - shorten field names
    - update the CLI initialization strategy

--- internal/waltr/app/app.go
+++ internal/waltr/app/app.go
@@ -2,86 +2,99 @@ package app
 
 import (
 	"fmt"
+	"github.com/fmjstudios/gopskit/pkg/core"
+	"github.com/fmjstudios/gopskit/pkg/fs"
+	"github.com/fmjstudios/gopskit/pkg/log"
+	"github.com/fmjstudios/gopskit/pkg/proc"
+	"github.com/spf13/cobra"
 	"time"
 
-	"github.com/fmjstudios/gopskit/pkg/cmd"
-	"github.com/fmjstudios/gopskit/pkg/common"
 	"github.com/fmjstudios/gopskit/pkg/kube"
-	"github.com/fmjstudios/gopskit/pkg/logger"
-	"github.com/fmjstudios/gopskit/pkg/platform"
 	"github.com/fmjstudios/gopskit/pkg/stamp"
 	"github.com/hashicorp/vault-client-go"
 )
 
 const (
-	Name             = "waltr"
-	DefaultNamespace = "vault"
-	DefaultLabel     = "app.kubernetes.io/name=vault"
+	Name         = "waltr"
+	DefaultLabel = "app.kubernetes.io/name=vault"
 )
 
-type Opt func(a *App)
+// Opt is configuration option for the application State
+type Opt func(a *State)
 
-// WithVaultOpts configures waltr's VaultClient instance with custom Options
-// from the vault-client-go package
-func WithVaultOpts(opts ...vault.ClientOption) Opt {
-	return func(a *App) {
-		a.VaultClient = cmd.Must(vault.New(opts...))
-	}
-}
+type CLIOpt func() func(a *State) *cobra.Command
 
-// type App is the implementation for the `waltr` command-line application
-type App struct {
-	*common.GOpsKitApp
+// State is the implementation for the `waltr` command-line application state
+type State struct {
+	*core.API
 
 	// VaultClient is the HashCorp first-party Go Vault HTTP client, which waltr
-	// uses for nearly all of it's functionality post high-availability setup
+	// uses for nearly all of its functionality
 	VaultClient *vault.Client
 }
 
-// New creates a newly initialized instance of the App type
-func New(opts ...Opt) *App {
+// New creates a newly initialized instance of the State type
+func New(opts ...Opt) (*State, error) {
 	var err error
 
-	platf := platform.New(platform.WithApp(Name))
-	log := logger.New()
-	defer func() {
-		err = log.Sync()
-	}()
-
+	platf, err := fs.Paths(fs.WithAppName(Name))
 	if err != nil {
-		log.Fatal("could not Sync logger", err)
+		return nil, err
 	}
 
-	exec := cmd.NewExecutor(cmd.WithInheritedEnv())
+	lgr := log.New()
+	defer func() {
+		err = lgr.Sync()
+	}()
+	if err != nil {
+		return nil, err
+	}
+
+	exec, err := proc.NewExecutor(proc.WithInheritedEnv())
+	if err != nil {
+		return nil, err
+	}
+
 	kc, err := kube.NewClient()
 	if err != nil {
-		log.Fatal("could not create Kubernetes Client", err)
+		return nil, fmt.Errorf("could not create kubernetes client: %v", err)
 	}
 
-	va := fmt.Sprintf("http://127.0.0.1:%s", kube.DefaultLocalPort)
-	vc, err := vault.New(vault.WithAddress(va), vault.WithRequestTimeout(60*time.Second))
+	// enforce HTTPS
+	va := fmt.Sprintf("https://127.0.0.1:%s", kube.DefaultLocalPort)
+	vc, err := vault.New(vault.WithAddress(va), vault.WithRequestTimeout(60*time.Second), vault.WithTLS(vault.TLSConfiguration{
+		InsecureSkipVerify: true,
+	}))
+
 	if err != nil {
-		log.Fatal("could not create Vault Client", err)
+		return nil, fmt.Errorf("could not create vault client: %v", err)
 	}
-
 	stamps := stamp.New()
 
-	a := &App{
-		GOpsKitApp: &common.GOpsKitApp{
-			Name:       Name,
-			Executor:   exec,
-			KubeClient: kc,
-			Logger:     log,
-			Platform:   platf,
-			Stamps:     stamps,
+	a := &State{
+		API: &core.API{
+			Name:  Name,
+			Exec:  exec,
+			Kube:  kc,
+			Log:   lgr,
+			Paths: platf,
+			Stamp: stamps,
 		},
 		VaultClient: vc,
 	}
 
-	// (re-)configure if the user wants do so
+	// (re-)configure if the user wants to do so
 	for _, o := range opts {
 		o(a)
 	}
 
-	return a
+	return a, nil
+}
+
+// WithVaultOpts configures waltr's VaultClient instance with custom Options
+// from the vault-client-go package
+func WithVaultOpts(opts ...vault.ClientOption) Opt {
+	return func(a *State) {
+		a.VaultClient = proc.Must(vault.New(opts...))
+	}
 }
