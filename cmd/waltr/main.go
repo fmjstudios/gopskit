19e6b5450b77daff1bfe33e41cb20e6ce633d4a9
FMJdev <info@fmj.dev>
2024-10-05 11:16:33 +0200

    chore(waltr): update `main.go` to new APIs

--- cmd/waltr/main.go
+++ cmd/waltr/main.go
@@ -4,13 +4,17 @@ import (
 	"github.com/fmjstudios/gopskit/internal/waltr/app"
 	"github.com/fmjstudios/gopskit/internal/waltr/cmd"
 	_ "github.com/fmjstudios/gopskit/pkg/stamp"
+	"log"
 )
 
 func main() {
-	a := app.New()
-	rCmd := cmd.NewRootCommand(a)
+	kern, err := app.New()
+	if err != nil {
+		log.Fatalf("could not initialize waltr application kernel: %v", err)
+	}
 
-	if err := rCmd.Execute(); err != nil {
-		a.Logger.Fatalf("%s exited with error: %v\n", a.Name, err)
+	cmdRoot := cmd.NewRootCommand(kern)
+	if err := cmdRoot.Execute(); err != nil {
+		kern.Log.Fatalf("%s exited with error: %v\n", kern.Name, err)
 	}
 }
