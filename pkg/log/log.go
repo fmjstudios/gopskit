17d19ae3637fb356cb3289182653254f68e3a415
FMJdev <info@fmj.dev>
2024-10-05 11:05:25 +0200

    refactor(pkg/logger): rename to `log`
    Rename the package and update the type to work asynchronously.
    
    - add new default Configuration (ProductionConfig)

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,8 +1,13 @@
-package logger
+package log
 
 import (
+	"context"
+	"fmt"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"sync"
 	"time"
 )
 
@@ -32,19 +37,77 @@ var (
 type Option func(config *zap.Config)
 
 // Logger is a type alias to Uber's zap logger, saving us from importing zap everywhere
+// and assuming the SugaredLogger's API results low effort logs later
 type Logger struct {
+	// log is the underlying zap Logger which is later embedded into
+	// our Logger
+	log *zap.Logger
+
+	// conf is private zap.Config for the underlying instance within log
+	conf *zap.Config
+
+	// lock is a Mutex which ensures that only one goroutine may modify the configuration
+	lock sync.Mutex
+
+	// Logger embeds a pointer zap.SugaredLogger to assume its' API
 	*zap.SugaredLogger
+}
+
+// Config builds a new zap.Config with optional Options for configuration
+func Config(opts ...zap.Option) *zap.Config {
+	c := zap.NewProductionConfig()
+	g := new(errgroup.Group)
+
+	// assert that it builds
+	g.Go(func() error {
+		_, err := c.Build(opts...)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	// crash if it isn't so
+	if err := g.Wait(); err != nil {
+		fmt.Printf("could not create zap.Config for Logger: %v", err)
+		os.Exit(1)
+	}
+
+	return &c
 }
 
 // New returns a newly built Logger including all or no Options for configuration
 func New(opts ...Option) *Logger {
-	cfg := DefaultConfig
+	l := &Logger{
+		conf: Config(),
+	}
+
+	l.lock.Lock()
+	defer l.lock.Unlock()
+
+	errg, _ := errgroup.WithContext(context.Background())
 	for _, opt := range opts {
-		opt(cfg)
+		errg.Go(func() error {
+			opt(l.conf)
+			return nil
+		})
+	}
+
+	// crash if it isn't so
+	if err := errg.Wait(); err != nil {
+		fmt.Printf("could not configure zap.Config for Logger: %v", err)
+		os.Exit(1)
+	}
+
+	lgr, err := l.conf.Build()
+	if err != nil {
+		fmt.Printf("could not build Config for Logger: %v", err)
 	}
 
 	return &Logger{
-		zap.Must(cfg.Build()).Sugar(),
+		log:           lgr,
+		SugaredLogger: lgr.Sugar(),
 	}
 }
 
